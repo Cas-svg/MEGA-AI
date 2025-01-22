6b092b170d914831d513f16679ed40c123b1aa9c
Cas-svg <edgarmakwasha0@gmail.com>
2025-01-22 14:34:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '263719495067';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '263719495067' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '263719495067' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || '6BojTL5I#lIllsH46KiJkfIVG44weH6hT6C_DL7_I5NMw0vpz3aA' ;  // put your session id here
 
 global.mods = []
 global.prems = []
